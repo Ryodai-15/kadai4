3ea4c442eb3923003556ece6bc1749d5f2ad536b
Ryodai-15 <gon.4629.1208@gmail.com>
2021-07-04 07:55:13 +0000

    [finish]kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,31 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォロー取得
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォロワー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 自分がフォローしている人
+  has_many :following_user, through: :follower, source: :followed
+  # 自分をフォローしている人
+  has_many :follower_user, through: :followed, source: :follower
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォロー確認をおこなう
+  def following?(user)
+    following_user.include?(user)
+  end
 
   validates :name,presence: true, length: {minimum: 2,maximum: 20}, uniqueness: true
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+   post 'follow/:id' => 'relationships#create', as: 'follow'
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
+
   get 'home/about' => "homes#about"
 
   root to:'homes#top'
@@ -8,8 +11,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
 
     resources :book_comments, only: [:create, :destroy]
-
   end
-  resources :users, only: [:new, :create, :index, :show, :edit, :update]
+
+  resources :users, only: [:new, :create, :index, :show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
 
 end
